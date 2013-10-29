1b681fe871973f785c247ef494ad8e0bdca4a5e7
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-29 14:45:11 +1200

    animation

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -77,13 +77,8 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
             }
             //Dont change the code before it
 
-            //Your code here about test explanation animation
-            //$content.find(".explanation").html("Something text for example");
-            //
-            //
-            //
-            //
-            //
+            var canvas = new PumpingStationsCanvas($content.find(".explanation")[0], checkioInput);
+            canvas.createCanvas(explanation);
 
 
             this_e.setAnimationHeight($content.height() + 60);
@@ -91,23 +86,64 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
         });
 
        
+        function PumpingStationsCanvas(dom, map) {
+            var format = Raphael.format;
 
-        var colorOrange4 = "#F0801A";
-        var colorOrange3 = "#FA8F00";
-        var colorOrange2 = "#FAA600";
-        var colorOrange1 = "#FABA00";
+            var colorOrange4 = "#F0801A";
+            var colorOrange3 = "#FA8F00";
+            var colorOrange2 = "#FAA600";
+            var colorOrange1 = "#FABA00";
 
-        var colorBlue4 = "#294270";
-        var colorBlue3 = "#006CA9";
-        var colorBlue2 = "#65A1CF";
-        var colorBlue1 = "#8FC7ED";
+            var colorBlue4 = "#294270";
+            var colorBlue3 = "#006CA9";
+            var colorBlue2 = "#65A1CF";
+            var colorBlue1 = "#8FC7ED";
 
-        var colorGrey4 = "#737370";
-        var colorGrey3 = "#9D9E9E";
-        var colorGrey2 = "#C5C6C6";
-        var colorGrey1 = "#EBEDED";
+            var colorGrey4 = "#737370";
+            var colorGrey3 = "#9D9E9E";
+            var colorGrey2 = "#C5C6C6";
+            var colorGrey1 = "#EBEDED";
 
-        var colorWhite = "#FFFFFF";
+            var colorWhite = "#FFFFFF";
+
+            var cell = 40;
+
+            var fullSizeX = map[0].length * cell,
+                fullSizeY = map.length * cell;
+
+            var paper = Raphael(dom, fullSizeX, fullSizeY, 0, 0);
+
+            var attrAxis = {"stroke": colorBlue4, "stroke-dasharray": "- ", "stroke-width": 1};
+            var attrPipe = {"stroke": colorBlue4, "stroke-width": 3};
+            var attrRect = {"stroke": colorBlue4, "stroke-width": 1, "fill": colorBlue1};
+
+
+            this.createCanvas = function(pipes) {
+                for (var i = 0; i <= map.length; i++){
+                    paper.path(format("M0,{0}H{1}", i * cell, fullSizeX)).attr(attrAxis);
+                }
+                for (i = 0; i <= map[0].length; i++){
+                    paper.path(format("M{0},0V{1}", i * cell, fullSizeY)).attr(attrAxis);
+                }
+                for (var row = 0; row < map.length; row++){
+                    for (var col = 0; col < map[0].length; col++){
+                        if (map[row][col] === 1) {
+                            paper.rect(col * cell, row * cell, cell, cell).attr(attrRect);
+                        }
+                    }
+                }
+                for (i = 0; i < pipes.length; i++) {
+                    var p = pipes[i];
+                    paper.path(format("M{0},{1}L{2},{3}"),
+                        p[1] * cell,
+                        p[0] * cell,
+                        p[3] * cell,
+                        p[2] * cell
+                    ).attr(attrPipe);
+                }
+            }
+
+        }
         //Your Additional functions or objects inside scope
         //
         //
